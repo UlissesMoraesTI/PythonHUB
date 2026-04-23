8432a8df94eec088d3b0145cf370ac66e76358a0
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2026-04-22 21:21:03 -0400

    feat:Listas

---
+++ Listas.py
@@ -0,0 +1,15 @@
+carros = []
+print(carros)
+
+carros.append('Honda City')
+print(carros)
+
+carros.insert(0, 'Brasilia')
+print(carros)
+
+motos = ['Ultra', 'Fat', 'Iron']
+print(motos)
+
+
+
+
---
+++ dicionario_adicionar_campo.py
@@ -0,0 +1,25 @@
+pessoa = {
+    'nome': 'Ulisses',
+    'idade': 25,
+    'profissao': 'Analista',
+    'tecnologia': ['Cobol', 'Python', 'Java', 'React'],
+    'formacao': ['Bacharel Sistemas de Informação', 'Dados', 'Front End React'],
+    'pet': {
+        'nome': 'Sol',
+        'idade': 4,
+        'peso': '3kg'
+        }
+    }
+
+pessoa['ano_nascimento'] = 1974
+
+print(pessoa)
+
+print(pessoa.get('ano_nascimento'))
+
+
+
+
+
+
+
