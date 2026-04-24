f44d01fe901f1a1b2c4d567ecbb2ee9148fac3f9
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2026-04-24 06:35:06 -0400

    feat:listas sort

---
+++ Listas_sort.py
@@ -0,0 +1,18 @@
+carros = ['Santana', 'Veraneio', 'Brasilia', 'Fusca']
+print(carros)
+
+carros.sort()
+print(carros)
+
+numeros = [2, 6, 3]
+print(numeros)
+
+numeros.sort()
+print(numeros)
+
+
+
+
+
+
+
